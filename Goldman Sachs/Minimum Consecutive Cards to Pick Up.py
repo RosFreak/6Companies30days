024ddd7ae52ac9e1da879d21c692bdfd96717022
RosFreak <102175551+RosFreak@users.noreply.github.com>
2023-01-23 20:25:54 +0530

    psh

---
+++ Goldman Sachs/Minimum Consecutive Cards to Pick Up.py
@@ -0,0 +1,17 @@
+#problem link
+#https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/description/
+
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        HashMap={}
+        lengths=[]
+        for i,j in enumerate(cards):
+            if j in HashMap:
+                lengths.append(abs(HashMap[j]-i)+1)
+                HashMap[j]=i
+            else:
+                HashMap[j]=i
+        if len(lengths)>0:
+            return min(lengths)
+        else:
+            return -1
